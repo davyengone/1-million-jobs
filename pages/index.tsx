f0520774fb0562cded8457a8c25677bc7f14ff61
davyengone <davy@hackages.io>
2022-02-01 09:02:22 +0100

    starting point

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>The 1 Million Jobs Project</title>
+        <title>Hackages Venture</title>
         <meta name="description" content="Plan to create 1 Meaningful Million Jobs over time!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -17,37 +17,37 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Join the community by choosing the category you belong to{' '}
+          {"Here are a few projects/companies we're building and investing in."}
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>I work at one of your companies</h2>
+          <a href="https://hackages.io" className={styles.card}>
+            <h2>Hackages Learning Platform</h2>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Your courses helped me find a meaningful Job</h2>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>You got me a meaningful freelance Job</h2>
+          <a href="https://techjargon.io" className={styles.card}>
+            <h2>The Tech Jargon Game</h2>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://refactoring.coach"
             className={styles.card}
           >
-            <h2>One of your products provided me a meaningful Job</h2>
+            <h2>Refactoring Coach</h2>
+          </a>
+
+          <a
+            href="https://www.youtube.com/channel/UCKmzC1m6b-riW_H7VC0Xu4A"
+            className={styles.card}
+          >
+            <h2>The Growth Engineer Podcast</h2>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://davyengone.io"
+          href="https://davy.engineer.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
